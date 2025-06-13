a5efaae3db1a15790d61dcf847c44bbe5d563373
Seth Ameyaw <sethameyaw@Seths-MacBook-Pro.local>
2025-06-13 18:08:17 +0000

    Initial commit

---
+++ app/auth.tsx
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Dimensions} from 'react-native';
+import {useRouter} from 'expo-router';
+import * as LocalAuthentication from 'expo-local-authentication';
+import { Ionicons } from '@expo/vector-icons';
+import {LinearGradient} from 'expo-linear-gradient';
+
+const { width } = Dimensions.get("window");
+
+export default function AuthScreen(){
+
+    return(
+        <LinearGradient colors={[ ]}>
+
+        </LinearGradient>
+    )
+}
